759d63f2bd453a9c6e7c062545592575f774be74
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-05-25 21:00:33 +0700

    d

--- src/mods/base58/wasm.ts
+++ src/mods/base58/wasm.ts
@@ -1,5 +1,5 @@
 import type { Base58Wasm } from "@hazae41/base58.wasm"
-import { Box, Ref } from "@hazae41/box"
+import { Pin, Ref } from "@hazae41/box"
 import { BytesOrCopiable } from "libs/copiable/index.js"
 import { Adapter } from "./adapter.js"
 
@@ -11,9 +11,9 @@ export function fromWasm(wasm: typeof Base58Wasm) {
       return new Ref(bytesOrCopiable)
 
     if (bytesOrCopiable instanceof Uint8Array)
-      return Box.from(new Memory(bytesOrCopiable))
+      return Pin.from(new Memory(bytesOrCopiable))
 
-    return Box.from(new Memory(bytesOrCopiable.bytes))
+    return Pin.from(new Memory(bytesOrCopiable.bytes))
   }
 
   function encodeOrThrow(bytes: BytesOrCopiable) {
